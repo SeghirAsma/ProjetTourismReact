dfd5ab7d01a31c0f961974f9907d18fa1af336bb
unknown <asmaseghir1770@gmail.com>
2024-02-08 17:13:20 +0100

    v1.1

--- src/components/Contenu.js
+++ src/components/Contenu.js
@@ -6,7 +6,7 @@ import axios from 'axios'; // Importez Axios
 import '../App.css';
 import VideoCard from './VideoCard';
 import Snackbar from '@mui/material/Snackbar';
-
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 const FormComponent = () => {
   const [videoFile, setVideoFile] = useState(null);
@@ -14,6 +14,9 @@ const FormComponent = () => {
   const [videos, setVideos] = useState([]);
   const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [titleContenu, setTitleContenu] = useState('');
+  const [descriptionContenu, setDescriptionContenu] = useState('');
+
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -29,6 +32,121 @@ const FormComponent = () => {
 
     fetchVideos();
   }, []);
+
+const handleTitleChange = (event) => {
+  setTitleContenu(event.target.value);
+};
+const handleDescriptionChange = (event) => {
+  setDescriptionContenu(event.target.value);
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  if (videoFile && titleContenu && descriptionContenu) {
+    const formData = new FormData();
+    formData.append('titleContenu', titleContenu);
+    formData.append('descriptionContenu', descriptionContenu);
+    formData.append('videoContenuUrl', videoFile);
+
+    try {
+      const response = await axios.post('http://localhost:8080/api/contenus/add', formData,
+       {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      // Affichez le snackbar avec un message de succès
+      setIsSnackbarOpen(true);
+      setSnackbarMessage(response.status === 201 ? 'Vidéo ajoutée avec succès' : 'Erreur lors de l\'ajout de la vidéo');
+    } catch (error) {
+      setIsSnackbarOpen(true);
+      setSnackbarMessage('Erreur lors de l\'envoi de la requête');
+    }
+  }
+};
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setVideoFile(file);
+  };
+
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
+
+  const handleCancel = () => {
+    setShowForm(false);
+  };
+  
+
+  return (
+    <div>
+      <Button className="bouton-contenu bouton-rond" variant="contained"  onClick={toggleForm}>
+      <AddCircleIcon />  Ajouter un contenu
+      </Button>
+      {showForm && (
+        <div className="modal-container">
+      <Container component="main" maxWidth="xs" >
+        <Paper elevation={3} style={{ padding: 16, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography component="h1" variant="h5" style={{ fontWeight: 'bold' }}>
+            Ajout d'un contenu
+          </Typography>
+          <form onSubmit={handleSubmit} style={{ width: '100%', marginTop: 16 }} encType="multipart/form-data" >
+            <TextField
+              id="titleContenu" label="Title Contenu" name="title" variant="outlined" margin="normal" autoComplete="title" required fullWidth
+              value={titleContenu}   onChange={handleTitleChange}  />
+            <TextField
+              id="descriptionContenu" label="Description Contenu" name="description" variant="outlined" autoComplete="description"
+              margin="normal"  required fullWidth value={descriptionContenu} onChange={handleDescriptionChange}
+            />
+      
+            <input
+              type="file"
+              accept="video/*"
+              style={{ marginTop: 16 }}
+              onChange={handleFileChange}
+            />
+            <div className="button-container">
+              <Button type="submit" variant="outlined" color="success" startIcon={<CloudUploadIcon />} className="submit-button" >
+                Ajouter
+              </Button>
+              <Button type="button" variant="outlined" color="error" className="cancel-button" onClick={handleCancel}>
+                Annuler
+              </Button>
+            </div>
+          </form>
+        </Paper>
+      </Container>
+      </div>
+       )}
+       {/* Liste des vidéos ajoutées */}
+      <div className="video-list" style={{ width: '100%',display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        {videos.map((video) => (
+          <VideoCard
+            key={video.idContenu} 
+            title={video.titleContenu}
+            description={video.descriptionContenu}
+            videoContenuUrl={video.videoContenuUrl}
+          />
+        ))}
+      </div>
+      {isSnackbarOpen && (
+  <Snackbar
+    open={isSnackbarOpen}
+    autoHideDuration={6000}
+    onClose={() => setIsSnackbarOpen(false)}
+    message={snackbarMessage}
+    style={{ backgroundColor: 'rgb(34, 200, 34)' }}
+  />
+  
+)}
+
+    </div>
+  );
+};
+
+export default FormComponent;
 
 //   const convertVideoToBase64 = (file) => {
 //     return new Promise((resolve, reject) => {
@@ -50,110 +168,3 @@ const FormComponent = () => {
 //     const base64Video = await convertVideoToBase64(file);
 //     setVideoFile(base64Video);
 //   };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (videoFile) {
-        console.log('Vidéo sélectionnée:', videoFile);
-      const formData = new FormData();
-      formData.append('titleContenu', event.target.titleContenu.value);
-      formData.append('descriptionContenu', event.target.descriptionContenu.value);
-      formData.append('videoContenuUrl', videoFile);
-
-      try {
-        const response = await axios.post('http://localhost:8080/api/contenus/add', formData,{
-            // headers:{
-            //     'Content-Type': 'multipart/form-data',
-            // }
-        });
-
- // Affichez le snackbar avec un message de succès
- setIsSnackbarOpen(true);
- setSnackbarMessage(response.status === 201 ? 'Vidéo ajoutée avec succès' : 'Erreur lors de l\'ajout de la vidéo');
-      } catch (error) {
-        console.error('Erreur lors de l\'envoi de la requête', error.response);
-        setIsSnackbarOpen(true);
-        setSnackbarMessage('Erreur lors de l\'envoi de la requête');
-      }
-    }
-  };
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    setVideoFile(file);
-  };
-
-  const toggleForm = () => {
-    setShowForm(!showForm);
-  };
-
-  const handleCancel = () => {
-    setShowForm(false);
-  };
-  
-
-  return (
-    <div>
-      <Button className="bouton-contenu bouton-rond" variant="contained"  onClick={toggleForm}>
-        <AddCircleIcon />
-      </Button>
-      {showForm && (
-      <Container component="main" maxWidth="xs">
-        <Paper elevation={3} style={{ padding: 16, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <Typography component="h1" variant="h5">
-            Ajout d'un contenu
-          </Typography>
-          <form onSubmit={handleSubmit} style={{ width: '100%', marginTop: 16 }} encType="multipart/form-data">
-            <TextField
-              id="titleContenu" label="Title Contenu" name="title" variant="outlined" margin="normal" autoComplete="title" required fullWidth
-            />
-            <TextField
-              id="descriptionContenu" label="Description Contenu" name="description" variant="outlined" autoComplete="description"
-              margin="normal"  required
-            />
-            <input
-              type="file"
-              accept="video/*"
-              style={{ marginTop: 16 }}
-              onChange={handleFileChange}
-            />
-            <div className="button-container">
-              <Button type="submit" variant="contained" color="success" className="submit-button" >
-                Ajouter
-              </Button>
-              <Button type="button" variant="contained" color="error" className="cancel-button" onClick={handleCancel}>
-                Annuler
-              </Button>
-            </div>
-          </form>
-        </Paper>
-      </Container>
-       )}
-       {/* Liste des vidéos ajoutées */}
-      <div className="video-list" style={{ width: '100%',display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        {videos.map((video) => (
-          <VideoCard
-            key={video.idContenu} 
-            title={video.titleContenu}
-            description={video.descriptionContenu}
-            videoContenuUrl={video.videoContenuUrl}
-          />
-        ))}
-      </div>
-      {isSnackbarOpen && (
-  <Snackbar
-    open={isSnackbarOpen}
-    autoHideDuration={6000}
-    onClose={() => setIsSnackbarOpen(false)}
-    message={snackbarMessage}
-    style={{ backgroundColor: 'rgb(34, 200, 34)' }}
-  />
-)}
-
-    </div>
-  );
-};
-
-export default FormComponent;
-
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,13 +1,14 @@
 // VideoCard.jsx
 import '../App.css';
-
 import React from 'react';
 import { Card, CardContent, Typography } from '@mui/material';
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player'
 
 const VideoCard = ({ title, description, videoContenuUrl }) => {
+  const filename = videoContenuUrl.split('/').pop();
+  const videoUrl=`http://localhost:8080/api/contenus/videos/${encodeURIComponent(filename)}`
   return (
-    <Card style={{ width: '60%', height: 'auto', marginBottom: 16}}>
+    <Card style={{ width: '40%', height: 'auto', marginBottom: 16}}>
       <CardContent >
         <Typography variant="h5" component="div">
           {title}
@@ -15,10 +16,8 @@ const VideoCard = ({ title, description, videoContenuUrl }) => {
         <Typography color="text.secondary">
           {description}
         </Typography>
-        
-
         <ReactPlayer 
-          url={`http://localhost:8080/${videoContenuUrl}`}
+          url={videoUrl}
           controls={true}
           style={{ height: 'auto', width: '100%', maxWidth:'100%',alignItems: 'center'}}
         />
