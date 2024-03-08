bb06fe54e6ed8b874b5d2d63e6d9c62a1fcd48e2
unknown <asmaseghir1770@gmail.com>
2024-03-08 16:58:36 +0100

    v7.1

--- src/components/ProfileSettingsAdmin.js
+++ src/components/ProfileSettingsAdmin.js
@@ -1,6 +1,6 @@
 import Sidebar from './Sidebar';
 import React, {useState, useEffect, useRef} from 'react';
-import { Container, Typography, Paper, Grid, TextField, Button,Divider } from '@mui/material';
+import { Container, Typography, Paper, Grid, TextField, Button,Divider,Alert } from '@mui/material';
 import axios from 'axios';
 import SettingsIcon from '@mui/icons-material/Settings';
 import IconButton from '@mui/material/IconButton';
@@ -8,6 +8,16 @@ import IconButton from '@mui/material/IconButton';
 function Profile() {
   const [currentUserDetails, setCurrentUserDetails] = useState(null);
   const fileInputRef = useRef(null);
+  const [successAlert, setSuccessAlert] = useState(false);
+  const [updatedUser, setUpdatedUser] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+  const profileImageUrl = currentUserDetails?.userEntity?.profileImageUrl || '';
+  const filename = profileImageUrl ? profileImageUrl.split('/').pop() : '';
+  const imageUrl=`http://localhost:8099/api/users/images/${encodeURIComponent(filename)}`
+  const [selectedFileName, setSelectedFileName] = useState('');
 
   useEffect(() => {
     const fetchCurrentUserDetails = async () => {
@@ -21,6 +31,17 @@ function Profile() {
 
         const currentUserDetails = userDetailsResponse.data;
         setCurrentUserDetails(currentUserDetails);
+          console.log("deyails",currentUserDetails)
+         setUpdatedUser({
+          firstName: currentUserDetails.userEntity.firstName,
+          lastName: currentUserDetails.userEntity.lastName,
+          email: currentUserDetails.userEntity.email,
+          profileImageUrl: currentUserDetails.userEntity.profileImageUrl,
+        });
+// // Vérifiez si l'URL de l'image est correctement mise à jour
+// if (currentUserDetails.userEntity.profileImageUrl !== profileImageUrl) {
+//   setProfileImageUrl(currentUserDetails.userEntity.profileImageUrl);
+// }
 
       } catch (error) {
         console.error('Failed to fetch user details', error);
@@ -28,7 +49,7 @@ function Profile() {
     };
 
     fetchCurrentUserDetails();
-  }, []);  
+  }, [profileImageUrl]);  
 
   const handleImageClick = () => {
     if (fileInputRef.current) {
@@ -43,16 +64,92 @@ function Profile() {
       const reader = new FileReader();
 
       reader.onload = () => {
-        // Handle the selected image, you can set it in the state if needed
-        console.log(reader.result);
+        console.log('File read successfully:', reader.result);
+      // setProfileImageUrl(reader.result); 
+      setSelectedFileName(file.name); // Mettez à jour le nom du fichier
       };
-
       reader.readAsDataURL(file);
     }
   };
 
+  const handleInputChange = (field, value) => {
+    setUpdatedUser({
+      ...updatedUser,
+      [field]: value,
+    });
+  };
 
+  const handleSaveChanges = async () => {
+    try {
+      const accessToken = localStorage.getItem('token');
+      const response = await axios.put(
+        `http://localhost:8099/api/users/update/${currentUserDetails.userEntity.id}`,
+        updatedUser,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      setSuccessAlert(true);
+      setTimeout(() => {
+        setSuccessAlert(false);
+      }, 4000);
+
+//  if (response.data.userEntity && response.data.userEntity.profileImageUrl) {
+//   setProfileImageUrl(response.data.userEntity.profileImageUrl);
+// }
+      setCurrentUserDetails({
+        ...currentUserDetails,
+        userEntity: response.data,
+      });
+      
+    } catch (error) {
+      console.error('Failed to update user details', error);
+    }
+  };
   
+  const handleImageUpload = async () => {
+    try {
+      const accessToken = localStorage.getItem('token');
+      const formData = new FormData();
+      formData.append('profileImage', fileInputRef.current.files[0]);
+
+      const response = await axios.put(
+        `http://localhost:8099/api/users/update-profile/${currentUserDetails.userEntity.id}`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+      // setProfileImageUrl(response.data.profileImageUrl);  
+      // console.log("profile img",setProfileImageUrl)
+
+      console.log('Profile image updated:', response.data);
+      // Remplacez la source de l'image avec la nouvelle URL
+    setCurrentUserDetails({
+      ...currentUserDetails,
+      userEntity: {
+        ...currentUserDetails.userEntity,
+        profileImageUrl: response.data.profileImageUrl,
+      },
+    });
+    
+
+      setSuccessAlert(true);
+      setTimeout(() => {
+        setSuccessAlert(false);
+      }, 4000);
+      setSelectedFileName(null); 
+    } catch (error) {
+      console.error('Failed to update profile image', error);
+    }
+  };
+
   return (
     <div style={{ display: 'flex', flexDirection: 'row', marginTop: '80px' }}>
    <Sidebar />
@@ -61,7 +158,6 @@ function Profile() {
         <Grid item xs={12} md={4}>
           <Paper sx={{ p: 3, height:'525px' ,backgroundColor: '#fbf9f3'}}>
             <Typography variant="h6" gutterBottom style={{fontWeight:'bold', color:'#033568'}}>
-           
               Profile Details
             </Typography>
             <div style={{ textAlign: 'center' }}>
@@ -73,12 +169,11 @@ function Profile() {
                   id="imageInput"
                   ref={fileInputRef}
                 />
-                <img
-                  src="https://static.vecteezy.com/ti/vecteur-libre/p1/24191699-profil-icone-ou-symbole-dans-rose-et-blanc-couleur-vectoriel.jpg"
-                  alt=""
-                  style={{
-                    borderRadius: '50%',
-                    maxWidth: '100%',
+               <img
+        src={imageUrl || "https://www.shutterstock.com/image-vector/man-icon-vector-260nw-1040084344.jpg"}
+        alt=""
+                  style={{ borderRadius: '50%',
+                    maxWidth: '200px',
                     maxHeight: '150px',
                     cursor: 'pointer',
                   }}
@@ -86,12 +181,24 @@ function Profile() {
                 />
               
               <Typography variant="body2" color="textSecondary" gutterBottom>
-                JPG or PNG no larger than 5 MB
+              All types of pictures are allowed
               </Typography>
+              {selectedFileName &&(
+              <Typography variant="body2" color="textSecondary" gutterBottom>
+  {`Selected Image: ${selectedFileName}`}
+</Typography> )}
+              <Button
+                  variant="contained"
+                  type="button"
+                  style={{ backgroundColor: '#3bc01a', marginTop: '10px' }}
+                  onClick={handleImageUpload}
+                >
+                  Import new Picture
+                </Button>
            <Divider />
                  <Typography  variant="body2" color="textSecondary" style={{ padding: '15px' }} >
           {currentUserDetails && (
-        <div style={{ fontSize: '18px'}}>
+        <div style={{ fontSize: '17px'}}>
           <p><span style={{fontWeight:'bold', color:'#84405a'}}>Email:</span> {currentUserDetails.userEntity.email}</p>
           <p><span style={{fontWeight:'bold', color:'#84405a'}}>First Name:</span>  {currentUserDetails.userEntity.firstName}</p>
           <p><span style={{fontWeight:'bold', color:'#84405a'}}>Last Name: </span> {currentUserDetails.userEntity.lastName}</p>
@@ -100,9 +207,7 @@ function Profile() {
         </div>
       )}
         </Typography>
-              {/* <Button variant="contained" color="primary" fullWidth>
-                Upload new image
-              </Button> */}
+             
             </div>
           </Paper>
         </Grid>
@@ -117,14 +222,18 @@ function Profile() {
             {currentUserDetails && (
             <form>
               <TextField fullWidth  id="inputfirstname" variant="outlined" margin="normal"
-              value={currentUserDetails.userEntity.firstName}/>
+              value={updatedUser.firstName}
+              onChange={(e) => handleInputChange('firstName', e.target.value)}/>
                <TextField fullWidth id="inputlastname" variant="outlined" margin="normal"
-                value={currentUserDetails.userEntity.lastName}/>
+                value={updatedUser.lastName}
+                onChange={(e) => handleInputChange('lastName', e.target.value)}/>
                 <TextField fullWidth  id="inputemail" variant="outlined" margin="normal"
-                 value={currentUserDetails.userEntity.email}/>
+                 value={updatedUser.email}
+                 onChange={(e) => handleInputChange('email', e.target.value)}/>
                <TextField fullWidth label="Address" id="inputadress" variant="outlined" margin="normal"/>
               <TextField fullWidth label="Phone Number" id="inputadress" variant="outlined" margin="normal"/>
-              <Button variant="contained"  type="button" style={{backgroundColor:'#3bc01a'}}>
+              <Button variant="contained"  type="button" style={{backgroundColor:'#3bc01a'}}
+              onClick={handleSaveChanges}>
                 Save changes
               </Button>
              
@@ -133,6 +242,16 @@ function Profile() {
         </Grid>
       </Grid>
     </Container>
+    {successAlert && currentUserDetails &&(
+      <Alert
+        severity="success"
+        color="success"
+        onClose={() => setSuccessAlert(false)}
+        style={{ position: 'fixed', bottom: '20px', left: '50%', transform: 'translateX(-50%)', zIndex: 9999 }}
+      >
+     Dear {currentUserDetails.userEntity.firstName}, Your Profile is updated  successfully
+      </Alert>
+    )}
     </div>
   );
 }
